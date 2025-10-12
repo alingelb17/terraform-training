730a77a342f1dda9bc6e5c1995fa41c4a46d74f1
Albert Lingelbach <alingelb@akamai.com>
2025-10-12 13:08:42 -0400

    varaibles module exercise

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+# terraform.tfvars
+#   per variables module exercise
+#   can use via e.g 
+#     terraform plan -var-file terraform.tfvars
+
+edgerc_path="/Users/alingelb/.edgerc"
+config_section="terraform-tcpublic"
--- variables.tf
+++ variables.tf
@@ -1,11 +1,16 @@
 # variables.tf
+#   per variables module exercise
+#   can use via e.g 
+#     terraform plan -var "config_section=terraform-govtsvcsintlab"
 
 variable "edgerc_path" {
   type = string
   default = "/Users/alingelb/.edgerc"
+  description = "path the the edgerc file"
 }
 
 variable "config_section" {
   type = string
   default = "terraform-tcpublic"
+  description = "the configuration section within the edgerc file to use for API access"
 }
