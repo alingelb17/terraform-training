94d3a77f7e3744ac7e99d70daf0b2e9621e2a8bf
Albert Lingelbach <alingelb@akamai.com>
2025-10-12 12:51:39 -0400

    variablize edgerc_path and config_section

---
+++ variables.tf
@@ -0,0 +1,11 @@
+# variables.tf
+
+variable "edgerc_path" {
+  type = string
+  default = "/Users/alingelb/.edgerc"
+}
+
+variable "config_section" {
+  type = string
+  default = "terraform-tcpublic"
+}
